3856f9002b6450c745953948b80dce40d660cc90
Arif <md.arifuz.zaman@g.bracu.ac.bd>
2026-09-02 15:54:33 +0600

    add release

---
+++ Cargo Hello World/hello_world/target/release/deps/hello_world-cc7164f4f95af809.d
@@ -0,0 +1,5 @@
+/home/blackcat/Projects/Rust001/Cargo Hello World/hello_world/target/release/deps/hello_world-cc7164f4f95af809.d: src/main.rs
+
+/home/blackcat/Projects/Rust001/Cargo Hello World/hello_world/target/release/deps/hello_world-cc7164f4f95af809: src/main.rs
+
+src/main.rs:
---
+++ Cargo Hello World/hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/home/blackcat/Projects/Rust001/Cargo\ Hello\ World/hello_world/target/release/hello_world: /home/blackcat/Projects/Rust001/Cargo\ Hello\ World/hello_world/src/main.rs
