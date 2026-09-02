4708ee5fe059bc9c626ae6836602267467d1460f
Arif <md.arifuz.zaman@g.bracu.ac.bd>
2026-09-02 15:50:28 +0600

    Using cargo

---
+++ Cargo Hello World/hello_world/target/debug/deps/hello_world-d2a1e779ac77d9e8.d
@@ -0,0 +1,5 @@
+/home/blackcat/Projects/Rust001/Cargo Hello World/hello_world/target/debug/deps/hello_world-d2a1e779ac77d9e8.d: src/main.rs
+
+/home/blackcat/Projects/Rust001/Cargo Hello World/hello_world/target/debug/deps/hello_world-d2a1e779ac77d9e8: src/main.rs
+
+src/main.rs:
---
+++ Cargo Hello World/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/blackcat/Projects/Rust001/Cargo\ Hello\ World/hello_world/target/debug/hello_world: /home/blackcat/Projects/Rust001/Cargo\ Hello\ World/hello_world/src/main.rs
